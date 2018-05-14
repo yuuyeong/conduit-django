77d041b6930941040965e67327975e1f9d8bb0c7
EunJung-Seo <seo.eunjeong5@gmail.com>
2018-05-14 20:29:48 +0900

    Create custom user manager

---
+++ conduit/apps/authentication/models.py
@@ -0,0 +1,29 @@
+import jwt
+
+from datetime import datetime, timedelta
+
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionMixin
+)
+from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if username is None or email is None:
+            raise TypeError('Users must have a username and email address')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError('Superusers must have a password')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
