4acdecec35e1f2aae820efc00b306373a3feb89c
EunJung-Seo <seo.eunjeong5@gmail.com>
2018-05-16 21:10:35 +0900

    Create Login API View

--- conduit/apps/authentication/views.py
+++ conduit/apps/authentication/views.py
@@ -4,7 +4,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .renderers import UserJSONRenderer
-from .serializers import RegistrationSerializer
+from .serializers import (
+    LoginSerializer, RegistrationSerializer
+)
 
 
 class RegistrationAPIView(APIView):
@@ -19,3 +21,15 @@ class RegistrationAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data.get('user',{})
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
