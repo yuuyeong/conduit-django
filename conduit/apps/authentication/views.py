d0b0aface7b60633013a7c8a00d442683fd21785
EunJung-Seo <seo.eunjeong5@gmail.com>
2018-05-18 00:22:35 +0900

    Remove data key from argument and add serializer.save()

--- conduit/apps/authentication/views.py
+++ conduit/apps/authentication/views.py
@@ -43,14 +43,17 @@ class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
     serializer_class = UserSerializer
 
     def retrieve(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.user)
+        serializer = self.serializer_class(request.user)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, *args, **kwargs):
         serializer_data = request.data.get('user', {})
+
         serializer = self.serializer_class(
-          request.user, data=serializer_data, partial=True
+            request.user, data=serializer_data, partial=True
         )
         serializer.is_valid(raise_exception=True)
+        serializer.save()
+
         return Response(serializer.data, status=status.HTTP_200_OK)
