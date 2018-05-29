833a393d527d07b16f0a932ec47b527c413da3d7
EunJung-Seo <seo.eunjeong5@gmail.com>
2018-05-29 12:29:17 +0900

    Create ProfileRetrieveAPIView

--- conduit/apps/core/exceptions.py
+++ conduit/apps/core/exceptions.py
@@ -3,6 +3,7 @@ from rest_framework.views import exception_handler
 def core_exception_handler(exc, context):
     response = exception_handler(exc, context)
     handlers = {
+        'ProfileDoesNotExist': _handle_generic_error,
         'ValidationError': _handle_generic_error
     }
     exception_class = exc.__class__.__name__
